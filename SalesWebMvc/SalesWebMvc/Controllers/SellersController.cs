221dae2c86f9aefced56ad382894d6ef0a25dbd5
Leonardo-Nascimento <leonardot.i@hotmail.com>
2020-03-22 10:17:22 -0300

    criando o controller sellersde novo

---
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error(string message)
+        {
+            //var viewModel = new ErrorViewModel
+            //{
+            //    Message = message,
+            //    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            //};
+            return View();
+        }
+
+    }
+}
