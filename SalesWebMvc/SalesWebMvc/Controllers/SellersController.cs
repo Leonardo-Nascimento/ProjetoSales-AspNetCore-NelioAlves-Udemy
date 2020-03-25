db2d84348fc4a0ad452113a6aa627a5131088b9c
Leonardo Nascimento <leonardo.nascimento@fitbank.com.br>
2020-03-25 13:12:00 -0300

    Delete Seller

--- SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -42,6 +42,30 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerServices.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerServices.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
 
         public IActionResult Error(string message)
         {
