82a54de83637c86a4a88dbc363fb4dd924af6c2e
Leonardo Nascimento <leonardo.nascimento@fitbank.com.br>
2020-04-12 14:39:16 -0300

    adicionando validações

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -9,9 +9,14 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} requerido")]
+        [StringLength(60,MinimumLength = 3, ErrorMessage ="{0} Nome deve ter entre {2} e {1} caracteres!")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} requerido")]
+        [EmailAddress(ErrorMessage = "Coloque um email válido!")]
         public string Email { get; set; }
 
         [Display(Name = "Birth Date")]
@@ -21,6 +26,8 @@ namespace SalesWebMvc.Models
 
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} requerido")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
